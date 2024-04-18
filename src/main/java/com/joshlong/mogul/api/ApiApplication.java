2e2d71338a07d4067a5a87bf1685f5553ece0dc5
Josh Long <josh@joshlong.com>
2024-04-18 11:07:45 +0200

    up

--- src/main/java/com/joshlong/mogul/api/ApiApplication.java
+++ src/main/java/com/joshlong/mogul/api/ApiApplication.java
@@ -14,9 +14,6 @@ import java.time.format.DateTimeFormatter;
 public class ApiApplication {
 
 	public static void main(String[] args) {
-
-		// System.getenv().forEach((k, v) -> System.out.println("\t" + k + '=' + v));
-
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
