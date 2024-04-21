652ada260ee83683ae0b368d7bde8bde484541d6
Josh Long <josh@joshlong.com>
2024-04-21 12:50:36 +0200

    up

--- src/main/java/com/joshlong/mogul/api/ApiApplication.java
+++ src/main/java/com/joshlong/mogul/api/ApiApplication.java
@@ -6,11 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.IntegrationComponentScan;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
 
 @IntegrationComponentScan
 @EnableConfigurationProperties(ApiProperties.class)
@@ -22,17 +28,30 @@ public class ApiApplication {
 	}
 
 	@Bean
-	SecurityFilterChain myActuatorAwareOauth2ResourceServerConfiguration(HttpSecurity http) throws Exception {
-		return http//
-			.oauth2ResourceServer((rs) -> rs.jwt(Customizer.withDefaults()))//
-			.authorizeHttpRequests((authorize) -> authorize //
-				.requestMatchers(EndpointRequest.toAnyEndpoint())
-				.permitAll() //
-				.anyRequest()
-				.authenticated()//
-			)//
-			.build();
+	RouterFunction<ServerResponse> httpRouterResponse() throws Exception {
+		return route().GET("/hello", request -> ok().body(Map.of("message", "Hello, world!"))).build();
 	}
+
+	@Bean
+	SecurityFilterChain jwtSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authorize) -> authorize //
+			.requestMatchers(EndpointRequest.toAnyEndpoint())
+			.permitAll() //
+			.anyRequest()
+			.authenticated()//
+		);
+		http.oauth2ResourceServer((resourceServer) -> resourceServer.jwt(withDefaults()));
+		return http.build();
+	}
+
+	/*
+	 * // @Bean SecurityFilterChain
+	 * myActuatorAwareOauth2ResourceServerConfiguration(HttpSecurity http) throws
+	 * Exception { return http// .authorizeHttpRequests((authorize) -> authorize //
+	 * .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll() //
+	 * .anyRequest().authenticated()// )// .oauth2ResourceServer((rs) -> rs
+	 * .jwt(Customizer.withDefaults()) )// .build(); }
+	 */
 
 	@Bean
 	DateTimeFormatter dateTimeFormatter() {
