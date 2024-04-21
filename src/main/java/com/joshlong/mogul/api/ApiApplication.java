39e2c7592ddfdd47bf3c74a680e07b4bcf759ae5
Josh Long <josh@joshlong.com>
2024-04-21 12:32:36 +0200

    polish

--- src/main/java/com/joshlong/mogul/api/ApiApplication.java
+++ src/main/java/com/joshlong/mogul/api/ApiApplication.java
@@ -1,10 +1,15 @@
 package com.joshlong.mogul.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.time.format.DateTimeFormatter;
 
@@ -15,6 +20,16 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	SecurityFilterChain myActuatorAwareOauth2ResourceServerConfiguration(HttpSecurity http) throws Exception {
+		return http.oauth2ResourceServer((rs) -> rs.jwt(Customizer.withDefaults()))
+			.authorizeHttpRequests((authorize) -> authorize.requestMatchers(EndpointRequest.toAnyEndpoint())
+				.permitAll()
+				.anyRequest()
+				.authenticated())
+			.build();
 	}
 
 	@Bean
