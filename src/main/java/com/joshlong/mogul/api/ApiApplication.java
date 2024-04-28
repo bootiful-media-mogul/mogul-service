4ca22b04302a3f271818df7b79bfe7f2f9d986df
Josh Long <josh@joshlong.com>
2024-04-28 12:46:10 -0700

    up

--- src/main/java/com/joshlong/mogul/api/ApiApplication.java
+++ src/main/java/com/joshlong/mogul/api/ApiApplication.java
@@ -1,8 +1,11 @@
 package com.joshlong.mogul.api;
 
+import org.apache.commons.logging.LogFactory;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +13,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
 import java.time.format.DateTimeFormatter;
 import java.util.Set;
 
@@ -33,6 +38,14 @@ public class ApiApplication {
 				hints.reflection().registerType(c, mcs);
 		}
 
+	}
+
+	@Bean
+	ApplicationRunner validateDatabase(DataSource db) {
+		return args -> {
+			var count = JdbcClient.create(db).sql("select count(*) from mogul").query(Long.class).single();
+			LogFactory.getLog(ApiApplication.this.getClass()).info("the count is " + count);
+		};
 	}
 
 	public static void main(String[] args) {
