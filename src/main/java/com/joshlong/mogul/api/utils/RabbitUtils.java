f9bf129321c0fb9c10ff090a74d72ce7085ff59c
Josh Long <josh@joshlong.com>
2024-07-15 01:25:03 -0700

    up

--- src/main/java/com/joshlong/mogul/api/utils/RabbitUtils.java
+++ src/main/java/com/joshlong/mogul/api/utils/RabbitUtils.java
@@ -10,14 +10,12 @@ public abstract class RabbitUtils {
 	public static void defineDestination(AmqpAdmin amqpAdmin, String exchange, String queue, String routingKey) {
 
 		var q = QueueBuilder.durable(queue).build();
-		q.setShouldDeclare(true);
 		amqpAdmin.declareQueue(q);
 
 		var e = ExchangeBuilder.topicExchange(exchange).durable(true).build();
 		amqpAdmin.declareExchange(e);
 
 		var b = BindingBuilder.bind(q).to(e).with(routingKey).noargs();
-		b.setShouldDeclare(true);
 		amqpAdmin.declareBinding(b);
 	}
 
