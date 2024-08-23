393fe284b1296db99b6c532424fea73f9599c1a9
Josh Long <josh@joshlong.com>
2024-08-22 17:53:12 -0700

    up

--- src/main/java/com/joshlong/mogul/api/notifications/NotificationsController.java
+++ src/main/java/com/joshlong/mogul/api/notifications/NotificationsController.java
@@ -6,6 +6,7 @@ import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 import java.util.Map;
 import java.util.Queue;
@@ -36,11 +37,17 @@ class NotificationsController {
 		var currentMogulId = this.mogulService.getCurrentMogul().id();
 		var notificationEvents = this.events.computeIfAbsent(currentMogulId, mogulId -> new ConcurrentLinkedQueue<>());
 		var notification = notificationEvents.poll();
-		if (null != notification)
-			return Map.of("mogulId", notification.mogulId(), //
+		if (null != notification) {
+			Assert.notNull(currentMogulId, "the current mogul id should not be null");
+			Assert.state(notification.when().getTime() > 0, "the time must not be null");
+			Assert.state(StringUtils.hasText(notification.context()), "the context must not be null");
+			Assert.state(StringUtils.hasText(notification.key()), "the key must not be null");
+			Assert.state(StringUtils.hasText(notification.category()), "the category must not be null");
+			return Map.of("mogulId", currentMogulId, //
 					"when", notification.when().getTime(), //
 					"context", notification.context(), "key", notification.key(), "category", notification.category(),
 					"modal", notification.modal());
+		}
 		return null;
 	}
 
