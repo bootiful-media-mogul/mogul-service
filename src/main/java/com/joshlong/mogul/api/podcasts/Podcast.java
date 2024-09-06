e0c2af302a1bee266073e88d420312a936f941f5
Josh Long <josh@joshlong.com>
2024-09-05 18:24:56 -0700

    closes #14

--- src/main/java/com/joshlong/mogul/api/podcasts/Podcast.java
+++ src/main/java/com/joshlong/mogul/api/podcasts/Podcast.java
@@ -3,14 +3,5 @@ package com.joshlong.mogul.api.podcasts;
 import java.util.Date;
 import java.util.List;
 
-/**
- *
- * represents one of many possible episode feeds, or carriers for episodes, of logically
- * distinct things. al moguls have, at the very least, one podcast automatically assigned
- * to them.
- *
- * @param mogulId
- * @param title
- */
 public record Podcast(Long mogulId, Long id, String title, Date created, List<Episode> episodes) {
 }
