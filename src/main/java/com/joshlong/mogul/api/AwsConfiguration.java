3be61129f9b9fb854338759c9acda2aa3edb51d8
Josh Long <josh@joshlong.com>
2024-09-12 08:55:12 +0900

    service

--- src/main/java/com/joshlong/mogul/api/AwsConfiguration.java
+++ src/main/java/com/joshlong/mogul/api/AwsConfiguration.java
@@ -11,6 +11,11 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 class AwsConfiguration {
 
+	//
+	// todo why does this take so long to startup and could i
+	// fix it with a simply proxy? do i use this somewhere
+	// else in the startup? why doesn't @Async fix it?
+	//
 	@Bean
 	S3Client s3Client(ApiProperties api) {
 		var key = api.aws().accessKey();
