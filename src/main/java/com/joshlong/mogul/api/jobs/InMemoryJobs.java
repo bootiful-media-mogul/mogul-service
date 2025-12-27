5885fa3274d1d8bd1edec4e13eddfb6586a2947f
Josh Long <54473+joshlong@users.noreply.github.com>
2025-12-26 22:07:55 -0800

    jobs

--- src/main/java/com/joshlong/mogul/api/jobs/InMemoryJobs.java
+++ src/main/java/com/joshlong/mogul/api/jobs/InMemoryJobs.java
@@ -13,63 +13,66 @@ import java.util.concurrent.Executors;
 @Service
 class InMemoryJobs implements Jobs {
 
-    private final Map<String, Job> jobs;
+	private final Map<String, Job> jobs;
 
-    private final ApplicationEventPublisher publisher;
-    private final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
-    private final Map<Long, Map<String, Job>> jobsInFlight = new ConcurrentHashMap<>();
+	private final ApplicationEventPublisher publisher;
 
-    InMemoryJobs(Map<String, Job> jobs, ApplicationEventPublisher publisher) {
-        this.jobs = jobs;
-        this.publisher = publisher;
-    }
+	private final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
 
+	private final Map<Long, Map<String, Job>> jobsInFlight = new ConcurrentHashMap<>();
 
-    @Override
-    public Map<String, Job> jobs() {
-        return new ConcurrentHashMap<>(this.jobs);
-    }
+	InMemoryJobs(Map<String, Job> jobs, ApplicationEventPublisher publisher) {
+		this.jobs = jobs;
+		this.publisher = publisher;
+	}
 
-    @Override
-    public void launch(String jobName, Map<String, Object> context) throws JobLaunchException {
-        Assert.state(!context.isEmpty(), "you must provide at least a valid mogulId");
-        Assert.state( jobs.containsKey(jobName) , "the job name doesn't exist");
-        Assert.hasText(jobName, "there must be a valid jobName");
-        var mogulId = (Long) context.get(Job.MOGUL_ID_KEY);
-        var jobsMap = this.jobsInFlight.computeIfAbsent(mogulId, _ -> new ConcurrentHashMap<>());
-        if (jobsMap.containsKey(jobName)) {
-            throw new JobLaunchException("job-already-running");
-        }
-        executor.submit(new Runnable() {
-            @Override
-            public void run() {
+	@Override
+	public Map<String, Job> jobs() {
+		return new ConcurrentHashMap<>(this.jobs);
+	}
 
-            }
-        }) ;
+	@Override
+	public void launch(String jobName, Map<String, Object> context) throws JobLaunchException {
+		Assert.state(!context.isEmpty(), "you must provide at least a valid mogulId");
+		Assert.state(jobs.containsKey(jobName), "the job name doesn't exist");
+		Assert.hasText(jobName, "there must be a valid jobName");
+		var mogulId = (Long) context.get(Job.MOGUL_ID_KEY);
+		var jobsMap = this.jobsInFlight.computeIfAbsent(mogulId, _ -> new ConcurrentHashMap<>());
+		if (jobsMap.containsKey(jobName)) {
+			throw new JobLaunchException("job-already-running");
+		}
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
 
+			}
+		});
 
-    }
-    private Job.Result run (String jn, Map <String, Object> map) {
-        var job = jobs.get(jn);
-        try {
-         return    job.run( map);
-        } //
-        catch (Exception e) {
-            return Job.Result.error( map , e);
-        }
-    }
+	}
+
+	private Job.Result run(String jn, Map<String, Object> map) {
+		var job = jobs.get(jn);
+		try {
+			return job.run(map);
+		} //
+		catch (Exception e) {
+			return Job.Result.error(map, e);
+		}
+	}
+
 }
 
 class JobLaunchException extends Exception {
 
-    private final String message;
+	private final String message;
 
-    JobLaunchException(String message) {
-        super(message);
-        this.message = message;
-    }
+	JobLaunchException(String message) {
+		super(message);
+		this.message = message;
+	}
 
-    public String getMessage() {
-        return this.message;
-    }
+	public String getMessage() {
+		return this.message;
+	}
+
 }
--- src/main/java/com/joshlong/mogul/api/jobs/JobsController.java
+++ src/main/java/com/joshlong/mogul/api/jobs/JobsController.java
@@ -9,44 +9,42 @@ import org.springframework.stereotype.Controller;
 import java.util.HashMap;
 import java.util.Map;
 
-
 @Controller
 class JobsController {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
-    private final Jobs jobs;
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
-    JobsController(Jobs jobs) {
-        this.jobs = jobs;
-    }
+	private final Jobs jobs;
 
-    @MutationMapping
-    boolean launch(String jobName, Map<String, Object> context) {
-        try {
-            this.jobs.launch(jobName, context);
-            this.log.info("launched {} with {}", jobName, context);
-        }//
-        catch (JobLaunchException jobLaunchException) {
-            log.warn("could not launch the job {} with context {}",
-                    jobName, context);
-            return false;
-        }
-        return true;
-    }
+	JobsController(Jobs jobs) {
+		this.jobs = jobs;
+	}
 
-    @QueryMapping
-    Map<String, JobView> jobs() {
-        var jobs = this.jobs.jobs();
-        var response = new HashMap<String, JobView>();
-        for (var j : jobs.entrySet()) {
-            var value = j.getValue();
-            response.put(j.getKey(), new JobView(j.getKey(),
-                    value.requiredContextAttributes().toArray(new String[0])));
-        }
-        return response;
-    }
+	@MutationMapping
+	boolean launch(String jobName, Map<String, Object> context) {
+		try {
+			this.jobs.launch(jobName, context);
+			this.log.info("launched {} with {}", jobName, context);
+		} //
+		catch (JobLaunchException jobLaunchException) {
+			log.warn("could not launch the job {} with context {}", jobName, context);
+			return false;
+		}
+		return true;
+	}
 
-    record JobView(String name, String[] requiredContextAttributes) {
-    }
+	@QueryMapping
+	Map<String, JobView> jobs() {
+		var jobs = this.jobs.jobs();
+		var response = new HashMap<String, JobView>();
+		for (var j : jobs.entrySet()) {
+			var value = j.getValue();
+			response.put(j.getKey(), new JobView(j.getKey(), value.requiredContextAttributes().toArray(new String[0])));
+		}
+		return response;
+	}
+
+	record JobView(String name, String[] requiredContextAttributes) {
+	}
 
 }
