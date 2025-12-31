2a2653f4e063605f601f0eae1fe28a274463bd25
Josh Long <54473+joshlong@users.noreply.github.com>
2025-12-31 19:10:07 +0800

    jobs

--- src/main/java/com/joshlong/mogul/api/podcasts/jobs/PodcastIndexerJob.java
+++ src/main/java/com/joshlong/mogul/api/podcasts/jobs/PodcastIndexerJob.java
@@ -19,121 +19,122 @@ import java.util.concurrent.Executors;
 @Component
 class PodcastIndexerJob implements Job {
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
-	private final int concurrency = this.concurrency();
+    private final int concurrency = this.concurrency();
 
-	private final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+    private final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
 
-	private final PodcastService podcastService;
+    private final PodcastService podcastService;
 
-	private final SearchService searchService;
+    private final SearchService searchService;
 
-	PodcastIndexerJob(PodcastService podcastService, SearchService searchService) {
-		this.podcastService = podcastService;
-		this.searchService = searchService;
-	}
+    PodcastIndexerJob(PodcastService podcastService, SearchService searchService) {
+        this.podcastService = podcastService;
+        this.searchService = searchService;
+    }
 
-	@Override
-	public Result run(Map<String, Object> context) throws Exception {
-		var mogulId = (Long) context.get(Job.MOGUL_ID_KEY);
-		var podcastId = (Long) context.get(Job.PODCAST_ID_KEY);
-		this.log.info("running for mogulId # {} and podcastId # {}", mogulId, podcastId);
-		this.log.warn("don't forget to uncomment me " +
-				"otherwise this job won't do anything!!");
-		// TODO
-//		this.doRun(mogulId, podcastId);
-		return Result.ok(context);
-	}
+    @Override
+    public Result run(Map<String, Object> context) throws Exception {
 
-	@Override
-	public @NonNull Set<String> requiredContextAttributes() {
-		var attrs = Job.super.requiredContextAttributes();
-		var all = new HashSet<>(attrs);
-		all.add(Job.PODCAST_ID_KEY);
-		return all;
-	}
+        this.log.info("running for context {}", context);
+        var mogulId = (Long) context.get(Job.MOGUL_ID_KEY);
+        var podcastId = (Long) context.get(Job.PODCAST_ID_KEY);
+        this.log.info("running for mogulId # {} and podcastId # {}", mogulId, podcastId);
+        this.log.warn("don't forget to uncomment me " +
+                "otherwise this job won't do anything!!");
+            // todo		this.doRun(mogulId, podcastId);
+        return Result.ok(context);
+    }
 
-	private void doRun(Long mogulId, Long podcastId) {
-		log.info("{} running for mogulId # {}", getClass().getName(), mogulId);
-		var allPodcastsByMogul = Set.of(this.podcastService.getPodcastById(podcastId));
-		log.info("there are {} podcasts for mogulId #{}", allPodcastsByMogul.size(), mogulId);
-		for (var podcast : allPodcastsByMogul) {
-			var episodes = podcastService.getPodcastEpisodesByPodcast(podcast.id(), false)
-				.stream()
-				.sorted(Comparator.comparingLong(Episode::id))
-				.toList();
-			log.info("episodes: found {} episodes for podcast {} for mogulId # {}", episodes.size(), podcast.id(),
-					mogulId);
+    @Override
+    public @NonNull Set<String> requiredContextAttributes() {
+        var attrs = Job.super.requiredContextAttributes();
+        var all = new HashSet<>(attrs);
+        all.add(Job.PODCAST_ID_KEY);
+        return all;
+    }
 
-			var mapDivided = this.divide(episodes);
-			var workQueuesCompletableFutures = new ArrayList<CompletableFuture<Void>>();
-			for (var entry : mapDivided.entrySet()) {
-				var cf = CompletableFuture.runAsync(new EpisodesRunnable(mogulId, podcast, entry.getValue()),
-						this.executor);
-				workQueuesCompletableFutures.add(cf);
-			}
-			CompletableFuture.allOf(workQueuesCompletableFutures.toArray(new CompletableFuture[0])).join();
-		}
-	}
+    private void doRun(Long mogulId, Long podcastId) {
+        log.info("{} running for mogulId # {}", getClass().getName(), mogulId);
+        var allPodcastsByMogul = Set.of(this.podcastService.getPodcastById(podcastId));
+        log.info("there are {} podcasts for mogulId #{}", allPodcastsByMogul.size(), mogulId);
+        for (var podcast : allPodcastsByMogul) {
+            var episodes = podcastService.getPodcastEpisodesByPodcast(podcast.id(), false)
+                    .stream()
+                    .sorted(Comparator.comparingLong(Episode::id))
+                    .toList();
+            log.info("episodes: found {} episodes for podcast {} for mogulId # {}", episodes.size(), podcast.id(),
+                    mogulId);
 
-	private int concurrency() {
-		var c = Math.max(Runtime.getRuntime().availableProcessors(), 6);
-		log.info("concurrency is {}", c);
-		return c;
-	}
+            var mapDivided = this.divide(episodes);
+            var workQueuesCompletableFutures = new ArrayList<CompletableFuture<Void>>();
+            for (var entry : mapDivided.entrySet()) {
+                var cf = CompletableFuture.runAsync(new EpisodesRunnable(mogulId, podcast, entry.getValue()),
+                        this.executor);
+                workQueuesCompletableFutures.add(cf);
+            }
+            CompletableFuture.allOf(workQueuesCompletableFutures.toArray(new CompletableFuture[0])).join();
+        }
+    }
 
-	private Map<Integer, Collection<Episode>> divide(Collection<Episode> episodes) {
-		var map = new ConcurrentHashMap<Integer, Collection<Episode>>();
-		var counter = 0;
-		for (var episode : episodes) {
-			var k = counter % concurrency;
-			map.computeIfAbsent(k, _ -> new HashSet<>()).add(episode);
-			counter += 1;
-		}
-		return map;
-	}
+    private int concurrency() {
+        var c = Math.max(Runtime.getRuntime().availableProcessors(), 6);
+        log.info("concurrency is {}", c);
+        return c;
+    }
 
-	class EpisodesRunnable implements Runnable {
+    private Map<Integer, Collection<Episode>> divide(Collection<Episode> episodes) {
+        var map = new ConcurrentHashMap<Integer, Collection<Episode>>();
+        var counter = 0;
+        for (var episode : episodes) {
+            var k = counter % concurrency;
+            map.computeIfAbsent(k, _ -> new HashSet<>()).add(episode);
+            counter += 1;
+        }
+        return map;
+    }
 
-		private final Long mogulId;
+    class EpisodesRunnable implements Runnable {
 
-		private final Podcast podcast;
+        private final Long mogulId;
 
-		private final Collection<Episode> episodes;
+        private final Podcast podcast;
 
-		EpisodesRunnable(Long mogulId, Podcast podcast, Collection<Episode> episodes) {
-			this.episodes = episodes;
-			this.podcast = podcast;
-			this.mogulId = mogulId;
-		}
+        private final Collection<Episode> episodes;
 
-		@Override
-		public void run() {
-			for (var ep : this.episodes) {
-				ingestEpisode(podcast, ep, mogulId);
-			}
-		}
+        EpisodesRunnable(Long mogulId, Podcast podcast, Collection<Episode> episodes) {
+            this.episodes = episodes;
+            this.podcast = podcast;
+            this.mogulId = mogulId;
+        }
 
-		private void ingestEpisode(Podcast podcast, Episode episode, Long mogulId) {
+        @Override
+        public void run() {
+            for (var ep : this.episodes) {
+                ingestEpisode(podcast, ep, mogulId);
+            }
+        }
 
-			if (!episode.complete()) {
-				log.info("skipping episode # {} because it is not 'complete'", episode.id());
-				return;
-			}
+        private void ingestEpisode(Podcast podcast, Episode episode, Long mogulId) {
 
-			var segments = podcastService.getPodcastEpisodeSegmentsByEpisode(episode.id());
-			log.info("there are {} episode segments for podcast episode {} for podcast {} for mogulId # {}",
-					segments.size(), episode.id(), podcast.id(), mogulId);
-			for (var segment : segments) {
-				if (segment.audio() == null || segment.producedAudio() == null) {
-					log.info("producedAudio is null, skipping.");
-					continue;
-				}
-				searchService.index(segment);
-			}
-		}
+            if (!episode.complete()) {
+                log.info("skipping episode # {} because it is not 'complete'", episode.id());
+                return;
+            }
 
-	}
+            var segments = podcastService.getPodcastEpisodeSegmentsByEpisode(episode.id());
+            log.info("there are {} episode segments for podcast episode {} for podcast {} for mogulId # {}",
+                    segments.size(), episode.id(), podcast.id(), mogulId);
+            for (var segment : segments) {
+                if (segment.audio() == null || segment.producedAudio() == null) {
+                    log.info("producedAudio is null, skipping.");
+                    continue;
+                }
+                searchService.index(segment);
+            }
+        }
+
+    }
 
 }
